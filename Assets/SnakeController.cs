5fca450118e2edda30d7b66ad25b0edd5e188011
Josh Williams <josh.williams@bluemedora.com>
2015-12-12 18:36:41 -0500

    Create classic snake controls with eased turning.

---
+++ Assets/SnakeController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SnakeController : MonoBehaviour {
+	private List<GameObject> segments = new List<GameObject>();
+	public GameObject segment;
+	public bool playerControlled = true;
+	public bool turnRight = false;
+	public bool turnLeft = false;
+		
+	void Start () {
+		GameObject g = (GameObject) GameObject.Instantiate (segment, transform.position, transform.rotation);
+		g.name = "Head";
+		g.transform.parent = this.transform;
+		segments.Add (g);
+		InvokeRepeating ("Move", .020f, .020f);
+		InvokeRepeating ("AddSegment", 1, 1);
+	}
+
+	void Update () {
+		if (playerControlled) {
+			turnRight = Input.GetKey (KeyCode.S);
+			turnLeft = Input.GetKey (KeyCode.A);
+		}
+	}
+
+	void Move() {
+		for(int i = segments.Count; i > 1; --i)
+		{
+			segments[i-1].transform.position = segments[i-2].transform.position;
+			segments[i-1].transform.rotation = segments[i-2].transform.rotation;
+		}
+		segments[0].transform.Translate(new Vector3(1,0,0) * 7 * Time.deltaTime);
+		if (turnLeft) {
+			segments [0].transform.localRotation *= Quaternion.Euler (0, 0, 5);
+		} else if (turnRight) {
+			segments [0].transform.localRotation *= Quaternion.Euler (0, 0, -5);
+		}
+	}
+
+	public void AddSegment(){
+		GameObject predecessor = segments [segments.Count - 1];
+		GameObject g = (GameObject)GameObject.Instantiate (segment, new Vector3(50, 50, 50), transform.rotation);
+		g.transform.parent = this.transform;
+		g.transform.localPosition += new Vector3 (-0.5f, 0, 0);
+		g.name = "Segment";
+		segments.Add (g);
+	}
+}
