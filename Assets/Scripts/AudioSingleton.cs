e98221e13b40e8fd522a4dcf573b92933d87613c
Cameron Jones <cameron.jones@bluemedora.com>
2015-12-12 18:06:36 -0500

    Audio added

---
+++ Assets/Scripts/AudioSingleton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSingleton : MonoBehaviour {
+	private static AudioSingleton instance = null;
+	private AudioSource gameAudio;
+
+	public static AudioSingleton Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void OnLevelWasLoaded() {
+		if (Application.loadedLevelName == "GameScene") {
+			AudioSource[] sources = gameObject.GetComponentsInChildren<AudioSource>();
+
+			foreach(AudioSource audio in sources) {
+				if(audio.name != "GameMusic") {
+					audio.Stop();
+				} else {
+					gameAudio = audio;
+				}
+			}
+
+			gameAudio.playOnAwake = true;
+			gameAudio.Play();
+		}
+	}
+}
