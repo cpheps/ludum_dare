76a91a6a467aecf34e66ea5ee264e7d32cba8917
Josh Williams <josh.williams@bluemedora.com>
2015-12-12 15:31:12 -0500

    Changed snake structure and added coil behavior

---
+++ Assets/Scripts/Snake Scripts/SnakeHeadMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeHeadMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.rotation = Quaternion.Lerp (transform.rotation, transform.rotation*Quaternion.Euler (0, 0, 30), Time.deltaTime * 5);
+		}
+
+		transform.Translate(new Vector3(1,0,0) * 7 * Time.deltaTime);
+	}
+}
