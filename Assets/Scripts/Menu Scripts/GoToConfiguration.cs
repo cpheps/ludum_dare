999bcefdda8f3d6579081990d081ebb528cd1aac
Cameron Jones <cameron.jones@bluemedora.com>
2015-12-12 13:07:21 -0500

    Configuration Menu stuff started to get added, but does not work well with input manager

---
+++ Assets/Scripts/Menu Scripts/GoToConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToConfiguration : MonoBehaviour {
+
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown (0)) {
+			//Application.LoadLevel("Configuration");
+		}
+	}
+}
