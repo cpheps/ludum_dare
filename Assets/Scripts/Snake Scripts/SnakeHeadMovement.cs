2e8857d224ab456018ae6b2c09037a850489a39c
Corbin Phelps <cphelps8907@gmail.com>
2015-12-12 15:52:22 -0500

    Fixed merge issues with snake clone

--- Assets/Scripts/Snake Scripts/SnakeHeadMovement.cs
+++ Assets/Scripts/Snake Scripts/SnakeHeadMovement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class SnakeHeadMovement : MonoBehaviour {
 
+	[SerializeField]
+	private int snakeSpeed = 7;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +17,6 @@ public class SnakeHeadMovement : MonoBehaviour {
 			transform.rotation = Quaternion.Lerp (transform.rotation, transform.rotation*Quaternion.Euler (0, 0, 30), Time.deltaTime * 5);
 		}
 
-		transform.Translate(new Vector3(1,0,0) * 7 * Time.deltaTime);
+		transform.Translate(new Vector3(1,0,0) * snakeSpeed * Time.deltaTime);
 	}
 }
