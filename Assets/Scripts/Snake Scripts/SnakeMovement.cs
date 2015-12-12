1b6d929f29344807faa60268b6449d8cb4b5ba1f
Corbin Phelps <cphelps8907@gmail.com>
2015-12-12 10:33:25 -0500

    Added snake prefab with basic movement

---
+++ Assets/Scripts/Snake Scripts/SnakeMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeMovement : MonoBehaviour {
+
+	[SerializeField]
+	private int snakeSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * snakeSpeed * Time.deltaTime);
+	}
+}
